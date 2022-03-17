23913cb3efb8a78b0dafb04a3e4aaff14359efeb
Alex Sherman <asherman.ca@gmail.com>
2022-03-17 11:26:34 -0700

    topbar fix

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,19 +1,40 @@
-import { getAuth } from 'firebase/auth'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function Topbar() {
-  const auth = getAuth()
+  // const auth = getAuth()
+  // console.log('auth1', auth)
+  // const [user, setUser] = useState(auth)
+  // console.log('auth2', user)
   const navigate = useNavigate()
-  const [currentUser, setCurrentUser] = useState(auth)
 
-  useEffect(() => {
-    setCurrentUser(auth.currentUser)
-  }, [auth.currentUser])
+  const auth = getAuth();
+  const [currentUser, setCurrentUser] = useState(null)
+
+
+  onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+     const uid = user.uid;
+     console.log('found', uid)
+     setCurrentUser(user)
+    // ...
+  } else {
+    // User is signed out
+    // ...
+    console.log('not found')
+    setCurrentUser(null)
+  }
+});
+
+  // useEffect(() => {
+  // }, [auth.currentUser])
 
   const onLogout = () => {
     auth.signOut()
-    setCurrentUser(null)
+    // setCurrentUser(null)
     navigate('/')
   }
 
@@ -23,11 +44,6 @@ function Topbar() {
         <li className="topbarListItem">
           <h1>Miyagi.com</h1>
         </li>
-        {/* {currentUser ?
-          <li className="topbarListItem" onClick={onLogout}>
-            Logout
-          </li> : <div>{' '}</div>
-        } */}
         {currentUser ?
           <li className="topbarListItem" onClick={onLogout}>
             X
