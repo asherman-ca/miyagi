393b5ebd5ba90e2685b606b8b243d505bf2c7f22
Alex Sherman <asherman.ca@gmail.com>
2022-03-29 13:15:49 -0700

    create ui init

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -4,27 +4,41 @@
   import Spinner from '../components/Spinner'
   import { toast } from 'react-toastify'
 
+
+
   export default function Create() {
+    const [loading, setLoading] = useState(false)
+
+    const onSubmit = async (e) => {
+      e.preventDefault()
+      console.log('submit')
+      setLoading(true)
+    }
+
+    if (loading) {
+      return <Spinner />
+    }
+
     return (
       <div className="pageContainer">
         <div className="contentContainer">
-          <form className="createFormContainer" onSubmit={() => console.log('hits')}>
-            <label className="createFormLabel">Image</label>
-            <input 
-              type='file' 
-              className='createFormInput'
-              id='imageUrl'
-              accept='.jpg,.png,.jpeg'
-            />
+          <form className="createFormContainer" onSubmit={onSubmit}>
             <label className="createFormLabel">Title</label>
             <input 
               type="text" 
               className="createFormInput" 
             />
-            <label className="createFormLabel">Note</label>
+            <label className="createFormLabel">Image</label>
             <input 
-              type="text" 
-              className="createFormInput" 
+              type='file' 
+              className="createFormImage"
+              id='imageUrl'
+              accept='.jpg,.png,.jpeg'
+            />
+            <label className="createFormLabel">Notes</label>
+            <textarea 
+              type="text"
+              className="createFormInput"
             />
             <label className="createFormLabel">Instagram</label>
             <input 
