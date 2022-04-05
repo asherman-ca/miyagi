f278740e2b0f19c51d955ab294e0140f261b1998
Alex Sherman <asherman.ca@gmail.com>
2022-04-05 12:24:09 -0700

    topbar done

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -2,6 +2,7 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import {  useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function Topbar() {
   const navigate = useNavigate()
@@ -31,20 +32,34 @@ function Topbar() {
 
   let authButton
   if(currentUser){
-    authButton = <li className="topbarListItem logoutButton" onClick={onLogout}>Sign Out</li>
+    // authButton = <li className="topbarListItem logoutButton" onClick={onLogout}>Sign Out</li>
+    authButton = <Nav.Link onClick={onLogout}>Sign Out</Nav.Link>
   }else{
-    authButton = <li className="topbarListItem logoutButton" onClick={() => navigate('/sign-in')}>Sign In</li>
+    // authButton = <li className="topbarListItem logoutButton" onClick={() => navigate('/sign-in')}>Sign In</li>
+    authButton = <Nav.Link onClick={() => navigate('/sign-in')}>Sign In</Nav.Link>
   }
 
   return (
-    <nav className="topbar">
-      <ul className="topbarListItems">
-        <li className="topbarListItem topbarTitle">
-          <h1>Miyagi.com</h1>
-        </li>
-        {loading ? <></> : authButton}
-      </ul>
-    </nav>
+    // <nav className="topbar">
+    //   <ul className="topbarListItems">
+    //     <li className="topbarListItem topbarTitle">
+    //       <h1>Miyagi.com</h1>
+    //     </li>
+    //     {loading ? <></> : authButton}
+    //   </ul>
+    // </nav>
+    <Navbar bg="light">
+      <Container>
+        <Navbar.Brand className="topbarTitle">
+          Miyagi.com
+        </Navbar.Brand>
+        <Navbar.Collapse id="basic.navbar-nav">
+          <Nav className="marginLeftAuto">
+            {loading ? <></> : authButton}
+          </Nav>
+        </Navbar.Collapse>  
+      </Container>
+    </Navbar>
   )
 }
 
