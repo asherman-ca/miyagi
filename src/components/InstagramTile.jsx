2493c3d6fe5b0249105e5f39305ed48ebd8d641d
Alex Sherman <asherman.ca@gmail.com>
2022-04-21 14:30:01 -0700

    fixed insta remove tile

--- src/components/InstagramTile.jsx
+++ src/components/InstagramTile.jsx
@@ -1,13 +1,13 @@
 import { Button } from 'react-bootstrap'
 
-const InstagramTile = ({postUser, currentUser, url, onYouTubeRemove}) => {
+const InstagramTile = ({postUser, currentUser, url, onInstaRemove}) => {
   return (
     <>
       <iframe src={`${url}embed`} height="480" frameborder="0" scrolling="yes" allowtransparency="true" className="previewFrame" />
       {postUser == currentUser &&
         <Button 
           variant="outline-danger" 
-          onClick={() => onYouTubeRemove(url)}>
+          onClick={() => onInstaRemove(url)}>
         Remove
         </Button>
       }
