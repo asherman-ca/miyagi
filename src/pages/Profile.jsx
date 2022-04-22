3dd8fb39d28e8c759bdc043cb7018df7481b0d53
Alex Sherman <asherman.ca@gmail.com>
2022-04-21 17:37:30 -0700

    cleanup

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -12,18 +12,12 @@ import Spinner from '../components/Spinner'
 function Profile() {
   const auth = getAuth()
   const [loading, setLoading] = useState(true)
-  const [changeDetails, setChangeDetails] = useState(false)
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
   })
   const [posts, setPosts] = useState(null)
-  const instaUrl = 'https://www.instagram.com/p/CaVixB0A206/'
-  const youtubeUrl = 'https://www.youtube.com/watch?v=3thEXIXTHyY'
-  const youtubeId = youtubeUrl.split("=")[1]
 
-
-  console.log('currnetuser', auth.currentUser)
   let profileImage = auth.currentUser.profileImage
   if (!profileImage && auth.currentUser.photoURL) {
     profileImage = auth.currentUser.photoURL
@@ -115,38 +109,6 @@ function Profile() {
         </Col>
       </Row>
     </Container>
-    // <div className="pageContainer">
-    //   <div className="contentContainer">
-    //     <header className="profileHeader">
-    //       <div className="profileImageContainer">
-    //         <button className="profileImageButton">
-    //           <img src={profileImage} alt="Change Profile Photo" className="profileImage" />
-    //         </button>
-    //       </div>
-
-    //       <div className="profileCard">
-    //         <div className="profileCardTitle">
-    //           <div className="profileCardName">
-    //             {formData.name}
-    //           </div>
-    //           <div className="profileCardEmail">
-    //             {formData.email}
-    //           </div>
-    //         </div>
-    //         <div className="profileCardDetails">
-    //           10 posts
-    //         </div>
-    //         <div className="profileCardFooter">
-    //           joined 2022
-    //         </div>
-    //       </div>
-    //     </header>
-    //     <iframe src={`${instaUrl}embed`} width="400" height="480" frameborder="0" scrolling="no" allowtransparency="true" />
-
-    //     <iframe width="943" height="530" src={`https://www.youtube.com/embed/${youtubeId}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
-    //   </div>
-
-    // </div>
   )
 }
 
