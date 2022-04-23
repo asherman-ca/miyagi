046219143bcbcca4206962bd7f3984e308252922
Alex Sherman <asherman.ca@gmail.com>
2022-04-22 22:19:08 -0700

    lowered margins

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -57,7 +57,7 @@ export default function Explore() {
     <Container>
       <Row className="exploreContainer">
         <Col md={{ span: 8, offset: 2 }}>
-        <Nav variant="tabs" defaultActiveKey="/" activeKey={params.exploreParam} className="mb-3">
+        <Nav variant="tabs" defaultActiveKey="/" activeKey={params.exploreParam} className="mb-2">
           <Nav.Item>
             <Nav.Link eventKey="/" href="/" className="tabLink">Newest</Nav.Link>
           </Nav.Item>
