b54fcacfdfad6836c2722df0f31e046b702be31f
Alex Sherman <asherman.ca@gmail.com>
2022-04-22 21:45:28 -0700

    embed cards

--- src/components/InstagramTile.jsx
+++ src/components/InstagramTile.jsx
@@ -1,17 +1,31 @@
-import { Button } from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
 
 const InstagramTile = ({postUser, currentUser, url, onInstaRemove}) => {
   return (
-    <>
+    <Card className="socialCardContainer">
+      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
       <iframe src={`${url}embed`} height="460" width="100%" frameborder="0" scrolling="yes" allowtransparency="true" className="previewFrame" />
-      {postUser == currentUser &&
-        <Button 
-          variant="outline-dark" 
-          onClick={() => onInstaRemove(url)}>
-        Remove
-        </Button>
-      }
-    </>
+      <Card.Body className="socialCardBody">
+        {postUser == currentUser &&
+          <Button 
+            variant="outline-dark" 
+            onClick={() => onInstaRemove(url)}>
+          Remove
+          </Button>
+        }
+      </Card.Body>
+    </Card>
+
+    // <>
+    //   <iframe src={`${url}embed`} height="460" width="100%" frameborder="0" scrolling="yes" allowtransparency="true" className="previewFrame" />
+      // {postUser == currentUser &&
+      //   <Button 
+      //     variant="outline-dark" 
+      //     onClick={() => onInstaRemove(url)}>
+      //   Remove
+      //   </Button>
+      // }
+    // </>
   )
 }
 
--- src/components/YouTubeTile.jsx
+++ src/components/YouTubeTile.jsx
@@ -1,17 +1,19 @@
-import { Button } from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
 
 const YouTubeTile = ({postUser, currentUser, url, onYouTubeRemove}) => {
   return (
-    <>
+    <Card className="socialCardContainer">
       <iframe height="315" width="100%" src={`https://www.youtube.com/embed/${url.split("=")[1]}`} title="YouTube video player" frameborder="0" className="previewFrame" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
-      {postUser == currentUser &&
-        <Button 
-          variant="outline-dark socialColumnDelete" 
-          onClick={() => onYouTubeRemove(url)}>
-        Remove
-        </Button>
-      }
-    </>
+      <Card.Body className="socialCardBody">
+        {postUser == currentUser &&
+          <Button 
+            variant="outline-dark socialColumnDelete" 
+            onClick={() => onYouTubeRemove(url)}>
+          Remove
+          </Button>
+        }
+      </Card.Body>
+    </Card>
   )
 }
 
