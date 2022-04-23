85e5d72c344e93d4564db051c5ddd3ce8ee60812
Alex Sherman <asherman.ca@gmail.com>
2022-04-22 20:54:34 -0700

    post item style

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -4,12 +4,14 @@ import { Card, Col } from 'react-bootstrap'
 function PostItem({post, id}) {
   return (
     <Col className="postItemCol" xs={4}>
-      <Link to={`/post/${id}`}>
-        <Card.Img className="cardImage" variant="top" src={post.imgUrls[0]}/>
-      </Link>
-      <Card.Body>
-        <Card.Title className="cardTitleText">{post.title}</Card.Title>
-      </Card.Body>
+      <Card className="postItemCard">
+        <Link to={`/post/${id}`}>
+          <Card.Img className="cardImage" variant="top" src={post.imgUrls[0]}/>
+        </Link>
+        <Card.Body className="postItemBody">
+          <Card.Title className="cardTitleText">{post.title}</Card.Title>
+        </Card.Body>
+      </Card>
     </Col>
   )
 }
