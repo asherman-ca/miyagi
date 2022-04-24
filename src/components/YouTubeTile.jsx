edaf1502b3a0eaed93519d65572f57e34ab4ea65
Alex Sherman <asherman.ca@gmail.com>
2022-04-23 19:13:29 -0700

    youtube tile fix

--- src/components/YouTubeTile.jsx
+++ src/components/YouTubeTile.jsx
@@ -3,7 +3,7 @@ import { Button, Card } from 'react-bootstrap'
 const YouTubeTile = ({postUser, currentUser, url, onYouTubeRemove}) => {
   return (
     <Card className="socialCardContainer">
-      <iframe height="315" width="100%" src={`https://www.youtube.com/embed/${url.split("=")[1]}`} title="YouTube video player" frameborder="0" className="previewFrame" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+      <iframe height="315" width="100%" src={`https://www.youtube.com/embed/${url.split('').slice(url.length-11,url.length).join('')}`} title="YouTube video player" frameborder="0" className="previewFrame" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
       <Card.Body className="socialCardBody">
         {postUser == currentUser &&
           <Button 
