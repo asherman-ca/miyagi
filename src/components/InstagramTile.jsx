6b9f76e5b0c5a50c62228f9fbebf41ecf4c04296
Alex Sherman <asherman.ca@gmail.com>
2022-04-23 19:03:45 -0700

    instagram tile fix

--- src/components/InstagramTile.jsx
+++ src/components/InstagramTile.jsx
@@ -3,8 +3,7 @@ import { Button, Card } from 'react-bootstrap'
 const InstagramTile = ({postUser, currentUser, url, onInstaRemove}) => {
   return (
     <Card className="socialCardContainer">
-      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-      <iframe src={`${url}embed`} height="460" width="100%" frameborder="0" scrolling="yes" allowtransparency="true" className="previewFrame" />
+      <iframe src={`${url.split('?')[0]}embed`} height="460" width="100%" frameborder="0" scrolling="yes" allowtransparency="true" className="previewFrame" />
       <Card.Body className="socialCardBody">
         {postUser == currentUser &&
           <Button 
@@ -15,17 +14,6 @@ const InstagramTile = ({postUser, currentUser, url, onInstaRemove}) => {
         }
       </Card.Body>
     </Card>
-
-    // <>
-    //   <iframe src={`${url}embed`} height="460" width="100%" frameborder="0" scrolling="yes" allowtransparency="true" className="previewFrame" />
-      // {postUser == currentUser &&
-      //   <Button 
-      //     variant="outline-dark" 
-      //     onClick={() => onInstaRemove(url)}>
-      //   Remove
-      //   </Button>
-      // }
-    // </>
   )
 }
 
