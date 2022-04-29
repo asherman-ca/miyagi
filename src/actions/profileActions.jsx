e59e7fe274e9c12c82e43c1164b4241750b7938e
Alex Sherman <asherman.ca@gmail.com>
2022-04-29 14:33:01 -0700

    action imports

---
+++ src/actions/profileActions.jsx
@@ -0,0 +1,134 @@
+import { db } from '../firebase.config'
+import { updateDoc, doc, collection, getDocs, query, where, orderBy, limit, writeBatch } from 'firebase/firestore'
+import { toast } from 'react-toastify'
+import { updateProfile } from 'firebase/auth'
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL
+} from 'firebase/storage'
+import { v4 as uuidv4 } from 'uuid'
+
+const onNameSubmit = async (e, auth, nameForm, posts, handleEditClose) => {
+  console.log('nameform', nameForm)
+  e.preventDefault()
+  if (auth.currentUser?.uid === 'cvT4fO1bQIR8HykmCmHYz82IlAu1') {
+    toast.error('Demo account locked')
+  } else {
+  const existingUserRef = collection(db, 'users')
+  const q = query(
+    existingUserRef,
+    where('name', '==', nameForm),
+    limit(10)
+  )
+  
+  const existingUserSnap = await getDocs(q)
+  if(existingUserSnap.empty){
+    await updateProfile(auth.currentUser, {
+      displayName: nameForm,
+    })
+
+    const userRef = doc(db, 'users', auth.currentUser.uid)
+    await updateDoc(userRef, {
+      name: nameForm
+    })
+
+    if(posts.length > 0) {
+      const batch = writeBatch(db)
+      posts.forEach((post) => {
+        const postRef = doc(db, 'posts', post.id)
+        batch.update(postRef, {userName: nameForm})
+      })
+      await batch.commit()
+    }
+    
+    toast.success('Name updated')
+    handleEditClose()
+  } else {
+    toast.error('Name unavailable')
+  }
+  }
+}
+
+const onNameChange = (e, setNameForm) => {
+  e.preventDefault()
+  setNameForm(() => (e.target.value))
+}
+
+const onImageSubmit = async (e, auth, setLoading, urlForm, handleClose) => {
+  e.preventDefault()
+  if (auth.currentUser?.uid === 'cvT4fO1bQIR8HykmCmHYz82IlAu1') {
+    toast.error('Demo account locked')
+  } else {
+  setLoading(true)
+  const storeImage = async (image) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage()
+      const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+      const storageRef = ref(storage, 'images/' + fileName)
+
+      const uploadTask = uploadBytesResumable(storageRef, image)
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          console.log('Upload is ' + progress + '% done')
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused')
+              break
+            case 'running':
+              console.log('Upload is running')
+              break
+            default:
+              break  
+          }
+        },
+        (error) => {
+          reject(error)
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL)
+          })
+        }
+      )
+    })
+  }
+
+  const imgUrls = await Promise.all(
+    [...urlForm].map((image) => storeImage(image))
+  ).catch(() => {
+    setLoading(false)
+    toast.error('Images not uploaded')
+    return
+  })
+
+  await updateProfile(auth.currentUser, {
+    photoURL: imgUrls[0],
+  })
+
+  const userRef = doc(db, 'users', auth.currentUser.uid)
+  await updateDoc(userRef, {
+    imageUrl: imgUrls[0]
+  })
+
+  setLoading(false)  
+  handleClose()
+  toast.success('Image saved')
+  }
+}
+
+const onImageUpdate = (e, setUrlForm) => {
+  e.preventDefault()
+  setUrlForm(() => (e.target.files))
+}
+
+
+export { onNameChange, onNameSubmit, onImageSubmit, onImageUpdate }
